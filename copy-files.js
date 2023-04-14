e105c0ff113396838b90dd00270f9537f89279bc
TheGodFathers <yusup.kejarid@gmail.com>
2023-04-15 06:22:17 +0700

    update

--- copy-files.js
+++ copy-files.js
@@ -5,3 +5,6 @@ fs.emptyDirSync('berkas/build');
 
 // Copy seluruh file dari direktori asal ke direktori tujuan
 fs.copySync('public/build', 'berkas/build');
+fs.copySync('public/sitemap.xml', 'berkas/sitemap.xml');
+fs.copySync('public/images', 'berkas/images');
+fs.copySync('public/audios', 'berkas/audios');
