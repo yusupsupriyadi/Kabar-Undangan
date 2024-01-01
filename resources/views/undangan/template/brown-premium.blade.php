7c5bd8fc2cd6f5553f1f233e8ff2c4830afcd310
yusup supriyadi <yusupsupriyadistudio@gmail.com>
2024-01-01 12:55:04 +0700

    Refactor Google Maps links in template files

--- resources/views/undangan/template/brown-premium.blade.php
+++ resources/views/undangan/template/brown-premium.blade.php
@@ -228,17 +228,19 @@
                             </div>
 
                         </div>
-                        <a id="gps-akad" title="google maps lokasi akad" href="{{ $data['setting_akad_api']['google_maps'] }}" target="blank" class="mx-auto mt-8 flex max-w-[10.5rem] items-center gap-2 rounded-xl bg-[#332216] p-3">
-                            <svg class="h-6 w-6 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <line x1="18" y1="6" x2="18" y2="6.01" />
-                                <path d="M18 13l-3.5 -5a4 4 0 1 1 7 0l-3.5 5" />
-                                <polyline points="10.5 4.75 9 4 3 7 3 20 9 17 15 20 21 17 21 15" />
-                                <line x1="9" y1="4" x2="9" y2="17" />
-                                <line x1="15" y1="15" x2="15" y2="20" />
-                            </svg>
-                            <h3 class="text-md text-white">Google Maps</h3>
-                        </a>
+                        @if ($data['setting_akad_api']['google_maps'] !== null)
+                            <a id="gps-akad" title="google maps lokasi akad" href="{{ $data['setting_akad_api']['google_maps'] }}" target="blank" class="mx-auto mt-8 flex max-w-[10.5rem] items-center gap-2 rounded-xl bg-[#332216] p-3">
+                                <svg class="h-6 w-6 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <line x1="18" y1="6" x2="18" y2="6.01" />
+                                    <path d="M18 13l-3.5 -5a4 4 0 1 1 7 0l-3.5 5" />
+                                    <polyline points="10.5 4.75 9 4 3 7 3 20 9 17 15 20 21 17 21 15" />
+                                    <line x1="9" y1="4" x2="9" y2="17" />
+                                    <line x1="15" y1="15" x2="15" y2="20" />
+                                </svg>
+                                <h3 class="text-md text-white">Google Maps</h3>
+                            </a>
+                        @endif
                     </div>
                 </div>
 
@@ -276,17 +278,19 @@
                             </div>
 
                         </div>
-                        <a id="gps-resepsi" title="google maps lokasi resepsi" href="{{ $data['setting_resepsi_api']['google_maps'] }}" target="blank" class="mx-auto mt-8 flex max-w-[10.5rem] items-center gap-2 rounded-xl bg-[#332216] p-3">
-                            <svg class="h-6 w-6 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <line x1="18" y1="6" x2="18" y2="6.01" />
-                                <path d="M18 13l-3.5 -5a4 4 0 1 1 7 0l-3.5 5" />
-                                <polyline points="10.5 4.75 9 4 3 7 3 20 9 17 15 20 21 17 21 15" />
-                                <line x1="9" y1="4" x2="9" y2="17" />
-                                <line x1="15" y1="15" x2="15" y2="20" />
-                            </svg>
-                            <h3 class="text-md text-white">Google Maps</h3>
-                        </a>
+                        @if ($data['setting_resepsi_api']['google_maps'] !== null)
+                            <a id="gps-resepsi" title="google maps lokasi resepsi" href="{{ $data['setting_resepsi_api']['google_maps'] }}" target="blank" class="mx-auto mt-8 flex max-w-[10.5rem] items-center gap-2 rounded-xl bg-[#332216] p-3">
+                                <svg class="h-6 w-6 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <line x1="18" y1="6" x2="18" y2="6.01" />
+                                    <path d="M18 13l-3.5 -5a4 4 0 1 1 7 0l-3.5 5" />
+                                    <polyline points="10.5 4.75 9 4 3 7 3 20 9 17 15 20 21 17 21 15" />
+                                    <line x1="9" y1="4" x2="9" y2="17" />
+                                    <line x1="15" y1="15" x2="15" y2="20" />
+                                </svg>
+                                <h3 class="text-md text-white">Google Maps</h3>
+                            </a>
+                        @endif
                     </div>
                 </div>
             </div>
